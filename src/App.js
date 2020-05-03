77cfda1753bf0b2b26574de100aaf1b86bd8813a
Ivan Martinez Vega <ivanvega08@outlook.com>
2020-05-02 21:28:28 -0500

    Side bar added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import SideBar from './components/SideBar/SideBar';
+import Home from './containers/Home/Home';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <SideBar className="side-bar" />
+      <main>
+        <Route path="/" exact component={Home} />{/* 
+        <Route path="/history" component={FavoritesPage} />
+        <Route path="/performance" component={FavoritesPage} /> */}
+      </main>
+    </React.Fragment>
   );
 }
 
